dcf4df01af9ab222bb29dfd1ca380f45c313a24c
vagrant <vagrant@homestead>
2018-04-12 01:13:45 +0000

    User profile page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class PagesController extends Controller
 {
@@ -10,6 +11,9 @@ class PagesController extends Controller
 
     public function root()
     {
+//        dd(User::all());
+
+//        dd(User::class);  //这个只是路径
         return view('pages.root');
     }
 }
