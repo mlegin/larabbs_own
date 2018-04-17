c3789590bb1df9128e166a71fc294ccbfdc4b215
vagrant <vagrant@homestead>
2018-04-17 07:11:56 +0000

    User can create replay

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -24,13 +24,6 @@
                 </div>
             </div>
 
-            {{-- 用户回复列表 --}}
-            <div class="panel panel-default topic-reply">
-                <div class="panel-body">
-                    @include('topics._reply_box', ['topic' => $topic])
-                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
-                </div>
-            </div>
         </div>
 
         <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
@@ -69,6 +62,14 @@
                         </div>
                     @endcan
 
+                </div>
+            </div>
+
+            {{-- 用户回复列表 --}}
+            <div class="panel panel-default topic-reply">
+                <div class="panel-body">
+                    @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
                 </div>
             </div>
         </div>
